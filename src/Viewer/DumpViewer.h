7cef70b6eff572e98545b73f493a499a748df94a
zodiacc7 <126355039+zodiacc7@users.noreply.github.com>
2026-09-04 21:21:18 +0330

    Display decoded credential values
    Added showCredentials method to DumpViewer class.

--- src/Viewer/DumpViewer.h
+++ src/Viewer/DumpViewer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../Dump/MifareClassic.h"
+#include "../Credential/ProfileLoader.h"
 
 #include <vector>
 #include <cstdint>
@@ -13,6 +14,12 @@ public:
 
     void showCard(
         MifareClassic& card
+    );
+
+
+    void showCredentials(
+        MifareClassic& card,
+        ProfileLoader& profile
     );
 
 
