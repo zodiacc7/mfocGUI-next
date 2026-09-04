6b147f6e634714bd1d6e7b1dc9ac8acaa7849e29
zodiacc7 <126355039+zodiacc7@users.noreply.github.com>
2026-09-04 21:23:37 +0330

    Add credential value editing

--- src/Dump/MifareClassic.cpp
+++ src/Dump/MifareClassic.cpp
@@ -53,6 +53,40 @@ bool MifareClassic::loadDump(
 
 
 
+bool MifareClassic::saveDump(
+    std::string filename
+)
+{
+
+    std::ofstream file(
+        filename,
+        std::ios::binary
+    );
+
+
+    if(!file)
+        return false;
+
+
+    for(auto& block : blocks)
+    {
+
+        file.write(
+            reinterpret_cast<char*>(
+                block.data()
+            ),
+            BLOCK_SIZE
+        );
+
+    }
+
+
+    return true;
+
+}
+
+
+
 std::vector<uint8_t>
 MifareClassic::getBlock(
     int block
@@ -74,6 +108,38 @@ MifareClassic::getBlock(
 
 
 
+bool MifareClassic::writeBlock(
+    int block,
+    std::vector<uint8_t> data
+)
+{
+
+    if(
+        block < 0 ||
+        block >= blocks.size()
+    )
+    {
+        return false;
+    }
+
+
+    if(
+        data.size() != BLOCK_SIZE
+    )
+    {
+        return false;
+    }
+
+
+    blocks[block] = data;
+
+
+    return true;
+
+}
+
+
+
 BlockInfo MifareClassic::getBlockInfo(
     int block
 )
@@ -84,23 +150,16 @@ BlockInfo MifareClassic::getBlockInfo(
 
     if(block < 128)
     {
-
-        info.sector =
-            block / 4;
-
-        info.block =
-            block % 4;
-
+        info.sector = block / 4;
+        info.block = block % 4;
     }
     else
     {
-
         info.sector =
             32 + ((block - 128) / 16);
 
         info.block =
             (block - 128) % 16;
-
     }
 
 
@@ -135,28 +194,24 @@ SectorTrailer MifareClassic::getSectorTrailer(
         getBlock(block);
 
 
-    if(
-        data.size() != 16
-    )
-    {
+    if(data.size() != 16)
         return trailer;
-    }
 
 
     trailer.keyA.assign(
         data.begin(),
-        data.begin() + 6
+        data.begin()+6
     );
 
 
     trailer.accessBits.assign(
-        data.begin() + 6,
-        data.begin() + 10
+        data.begin()+6,
+        data.begin()+10
     );
 
 
     trailer.keyB.assign(
-        data.begin() + 10,
+        data.begin()+10,
         data.end()
     );
 
