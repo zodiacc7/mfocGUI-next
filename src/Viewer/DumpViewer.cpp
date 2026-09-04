620cb6a8b6523add0346fa7272d5b7353d96e44a
zodiacc7 <126355039+zodiacc7@users.noreply.github.com>
2026-09-04 20:48:09 +0330

    Update DumpViewer.cpp

--- src/Viewer/DumpViewer.cpp
+++ src/Viewer/DumpViewer.cpp
@@ -1,1 +1,40 @@
+#include "DumpViewer.h"
+#include <iostream>
 
+
+void DumpViewer::showStructure(int sectors)
+{
+
+    for(int s = 0; s < sectors; s++)
+    {
+
+        std::cout
+            << "Sector "
+            << s
+            << std::endl;
+
+
+        for(int b = 0; b < 4; b++)
+        {
+
+            if(b == 3)
+            {
+                std::cout
+                    << " Block "
+                    << b
+                    << " (Trailer)"
+                    << std::endl;
+            }
+            else
+            {
+                std::cout
+                    << " Block "
+                    << b
+                    << std::endl;
+            }
+
+        }
+
+    }
+
+}
