82b368d2caca0a332f7956609199703b598a9b14
zodiacc7 <126355039+zodiacc7@users.noreply.github.com>
2026-09-04 20:37:20 +0330

    Add JSON parsing in JsonParser to load fields
    Implemented JSON parsing to load credential fields from a specified file.

--- src/Credential/JsonParser.cpp
+++ src/Credential/JsonParser.cpp
@@ -1,4 +1,10 @@
 #include "JsonParser.h"
+#include "../../external/json/json.hpp"
+
+#include <fstream>
+
+
+using json = nlohmann::json;
 
 
 std::vector<CredentialField>
@@ -7,7 +13,49 @@ JsonParser::loadFields(std::string filename)
     std::vector<CredentialField> fields;
 
 
-    // JSON parsing will be implemented here
+    std::ifstream file(filename);
+
+
+    if(!file)
+        return fields;
+
+
+    json j;
+
+    file >> j;
+
+
+    for(auto& item : j["fields"])
+    {
+        CredentialField field;
+
+
+        field.name =
+            item["name"];
+
+
+        field.sector =
+            item["sector"];
+
+
+        field.block =
+            item["block"];
+
+
+        field.offset =
+            item["offset"];
+
+
+        field.size =
+            item["size"];
+
+
+        field.type =
+            item["type"];
+
+
+        fields.push_back(field);
+    }
 
 
     return fields;
