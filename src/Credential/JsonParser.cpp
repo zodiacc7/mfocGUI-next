c02ca7af70297f7d6be366bdd4a6849d0a59e707
zodiacc7 <126355039+zodiacc7@users.noreply.github.com>
2026-09-04 20:27:54 +0330

    Create JsonParser.cpp

---
+++ src/Credential/JsonParser.cpp
@@ -0,0 +1,14 @@
+#include "JsonParser.h"
+
+
+std::vector<CredentialField>
+JsonParser::loadFields(std::string filename)
+{
+    std::vector<CredentialField> fields;
+
+
+    // JSON parsing will be implemented here
+
+
+    return fields;
+}
