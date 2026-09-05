3015329726e22c3683ff336032e5f874c4eae51c
zodiacc7 <126355039+zodiacc7@users.noreply.github.com>
2026-09-05 13:38:06 +0330

    Fix MSVC PTRDIFF_MAX build issue

---
+++ src/Credential/CredentialDecoder.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "CredentialField.h"
+#include "../Dump/MifareClassic.h"
+
+#include <cstddef>
+#include <vector>
+#include <cstdint>
+#include <string>
+
+
+
+class CredentialDecoder
+{
+
+public:
+
+    std::vector<uint8_t> decode(
+        MifareClassic& card,
+        CredentialField field
+    );
+
+
+    bool encode(
+        MifareClassic& card,
+        CredentialField field,
+        std::vector<uint8_t> value
+    );
+
+
+    std::string decodeValue(
+        std::vector<uint8_t> data,
+        CredentialType type
+    );
+
+
+    std::vector<uint8_t> encodeValue(
+        std::string value,
+        CredentialType type,
+        int size
+    );
+
+};
